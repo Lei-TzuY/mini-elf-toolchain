b3baf7920c284f1886782801f739e6b1fec11432
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-19 16:59:13 +0800

    fix: apply rustfmt layout to preload comment parser

--- src/system_preload.rs
+++ src/system_preload.rs
@@ -42,7 +42,9 @@ impl SystemPreloadRequest {
         let entries = contents
             .lines()
             .flat_map(|line| {
-                let uncommented = line.split_once('#').map_or(line, |(prefix, _)| prefix);
+                let uncommented = line
+                    .split_once('#')
+                    .map_or(line, |(prefix, _)| prefix);
                 uncommented.split_ascii_whitespace()
             })
             .map(str::to_owned)
