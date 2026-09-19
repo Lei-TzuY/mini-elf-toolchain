b15f51257ad26a197591b858f963cd0dbb0180a3
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-19 15:59:53 +0800

    feat: parse system preload comments

--- src/system_preload.rs
+++ src/system_preload.rs
@@ -40,7 +40,11 @@ impl SystemPreloadRequest {
         })?;
 
         let entries = contents
-            .split_ascii_whitespace()
+            .lines()
+            .flat_map(|line| {
+                let uncommented = line.split_once('#').map_or(line, |(prefix, _)| prefix);
+                uncommented.split_ascii_whitespace()
+            })
             .map(str::to_owned)
             .collect::<Vec<_>>();
         for entry in &entries {
