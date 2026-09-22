eb5ed0c1934011accc81ba365ddc5f7b430da961
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-23 02:24:55 +0800

    feat: expose partial-link module

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,7 @@ pub mod object_symbols;
 pub mod ordered_inputs;
 pub mod output_image;
 pub mod permission_layout;
+pub mod partial_link;
 pub mod preload;
 mod program_headers;
 pub mod rela_apply;
