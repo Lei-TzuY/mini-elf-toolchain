f955e3a28d173e030d35c4f70f94f3994d24baeb
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-23 09:47:07 +0800

    feat: expose x86-64 GLOB_DAT relocation kind

--- src/x86_64_relocations.rs
+++ src/x86_64_relocations.rs
@@ -6,6 +6,7 @@ pub const R_X86_64_64: u32 = 1;
 pub const R_X86_64_PC32: u32 = 2;
 pub const R_X86_64_GOT32: u32 = 3;
 pub const R_X86_64_PLT32: u32 = 4;
+pub const R_X86_64_GLOB_DAT: u32 = 6;
 pub const R_X86_64_GOTPCREL: u32 = 9;
 pub const R_X86_64_32: u32 = 10;
 pub const R_X86_64_32S: u32 = 11;
