b635506b6252172a5b3d882a816d9a14ff6f6c64
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-25 03:46:32 +0800

    style: apply rustfmt to dynamic PIE RELRO integration

--- src/shared_object.rs
+++ src/shared_object.rs
@@ -1949,10 +1949,8 @@ fn link_loader_image(
             map_runtime_program_headers_with_dynamic_and_relros(image, dynamic, &[relro])
                 .map_err(SharedObjectError::Write)
         }
-        ((None, None), None) => {
-            map_runtime_program_headers_with_dynamic(image, dynamic)
-                .map_err(SharedObjectError::Write)
-        }
+        ((None, None), None) => map_runtime_program_headers_with_dynamic(image, dynamic)
+            .map_err(SharedObjectError::Write),
         _ => unreachable!("interpreter payload and path are constructed together"),
     }
 }
