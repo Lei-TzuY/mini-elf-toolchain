ee033c26248c55929ebd48e93ba5baa155842e77
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-26 00:56:57 +0800

    fix: share CRT start symbol index with emitter

--- src/crt_startup.rs
+++ src/crt_startup.rs
@@ -109,8 +109,10 @@ pub fn build_dynamic_pie_crt_startup_object() -> Vec<u8> {
         0,
         0,
     );
+    let start_symbol_offset =
+        symtab_offset + START_SYMBOL_INDEX as usize * ELF64_SYMBOL_SIZE;
     write_symbol(
-        &mut file[symtab_offset + ELF64_SYMBOL_SIZE..symtab_offset + 2 * ELF64_SYMBOL_SIZE],
+        &mut file[start_symbol_offset..start_symbol_offset + ELF64_SYMBOL_SIZE],
         start_name,
         (STB_GLOBAL << 4) | STT_FUNC,
         TEXT_SECTION_INDEX,
